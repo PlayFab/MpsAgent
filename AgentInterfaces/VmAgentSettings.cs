4f908309443caf4b686a1ef7d25b9995a1c73cc3
Brian Porter <bport@microsoft.com>
2021-06-18 16:21:32 -0700

    added VmAgentSettings class that can be used to allow VmAgent to request settings from ControlPlane

---
+++ AgentInterfaces/VmAgentSettings.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Azure.Gaming.AgentInterfaces
+{
+    public class VmAgentSettings
+    {
+        public bool ProcessCrashDumps { get; set; }
+
+        public bool SendTelemetry { get; set; }
+    }
+}

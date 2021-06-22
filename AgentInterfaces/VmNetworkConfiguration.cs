c743e3eb4e996a046be59d0efc283c7ad06a6854
Rakesh Varna <ravarna@microsoft.com>
2021-06-22 09:28:33 -0700

    Added comments.

--- AgentInterfaces/VmNetworkConfiguration.cs
+++ AgentInterfaces/VmNetworkConfiguration.cs
@@ -2,10 +2,19 @@
 {
     public class VmNetworkConfiguration
     {
+        /// <summary>
+        /// The public Ipv4 address that can be used to connect to the VM.
+        /// </summary>
         public string PublicIpv4Address { get; set; }
 
+        /// <summary>
+        /// The fully qualified domain name for the VM. Useful for scenarios involving IPv6 (where the Ipv4 address cannot be used).
+        /// </summary>
         public string Fqdn { get; set; }
 
+        /// <summary>
+        /// A list of endpoints on the VM which can be assigned to the game servers so that the game clients can connect to them.
+        /// </summary>
         public Endpoint[] Endpoints { get; set; }
     }
 

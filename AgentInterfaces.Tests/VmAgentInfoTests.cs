2a70c57218a808e4278be13f5da530f3eb974173
Rakesh Varna <ravarna@microsoft.com>
2021-06-22 11:17:24 -0700

    Fix unit test failure.

--- AgentInterfaces.Tests/VmAgentInfoTests.cs
+++ AgentInterfaces.Tests/VmAgentInfoTests.cs
@@ -49,6 +49,7 @@ namespace AgentInterfaces.Tests
                 nameof(VmAgentInfo.AssignmentId),
                 nameof(VmAgentInfo.SequenceNumber),
                 nameof(VmAgentInfo.IsUnassignable),
+                nameof(VmAgentInfo.NetworkConfiguration)
             };
 
             HashSet<string> propertyNames =

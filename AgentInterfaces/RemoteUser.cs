cfced12f6f3dc54964cbba110cb1f1fc0ebf3e1b
Rakesh Varna <ravarna@microsoft.com>
2021-08-04 00:54:59 -0700

    Add data model for remote user.

---
+++ AgentInterfaces/RemoteUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Azure.Gaming.AgentInterfaces
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RemoteUser
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime? ExpirationTime { get; set; }
+        
+        public string IpV4Address { get; set; }
+
+        public int Port { get; set; }
+    }
+}

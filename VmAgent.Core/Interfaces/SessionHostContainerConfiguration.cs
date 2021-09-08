89adc91d78bec572aaeb6c46dd4ae767cecd2119
Emma Park <emmapark@microsoft.com>
2021-09-08 15:17:26 -0700

    Updated GameLogsRootFolderContainer path in VM directory

--- VmAgent.Core/Interfaces/SessionHostContainerConfiguration.cs
+++ VmAgent.Core/Interfaces/SessionHostContainerConfiguration.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.Gaming.VmAgent.Core.Interfaces
         {
             // The VM host folder corresponding to the logFolderId gets mounted under this path for each container.
             // So the logFolderId itself isn't of much significance within the container.
-            return vmConfiguration.VmDirectories.GameLogsRootFolderContainer + Path.DirectorySeparatorChar;
+            return vmConfiguration.VmDirectories.GameLogsRootFolderContainer + Path.AltDirectorySeparatorChar;
         }
 
         protected override string GetSharedContentFolder(VmConfiguration vmConfiguration)

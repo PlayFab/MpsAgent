a13a799043f60f3ef63766e3541a12da0ceda483
Emma Park <emmapark@microsoft.com>
2021-09-16 15:33:37 -0700

    Revert "Revert "solving wrong revert commit""
    This reverts commit 9500220bb134bcdb1e8ae7f802390b192263f8b6.

---
+++ LocalMultiplayerAgent.UnitTest/MultiplayerServerManagerTestUtil.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Azure.Gaming.VmAgent.UnitTests
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Reflection;
+    using AgentInterfaces;
+    using Core.Interfaces;
+    using FluentAssertions;
+    using global::VmAgent.Core.Interfaces;
+    using LocalMultiplayerAgent.Config;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.Azure.Gaming.VmAgent.Core;
+    using Microsoft.Extensions.Logging.Abstractions;
+    using Moq;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using VisualStudio.TestTools.UnitTesting;
+
+    public static class MultiplayerServerManagerTestUtil
+    {
+        internal static readonly Guid LegacyTestTitleId = new Guid("0b430f66-a5e4-4cca-8390-4324e84cc574");
+        internal static readonly Guid TestTitleId = Guid.NewGuid();
+        internal static readonly Guid TestBuildId = Guid.NewGuid();
+        internal static readonly Guid TestVmID = Guid.NewGuid();
+        internal static readonly string settingFilePath = "MultiplayerSettings.json";
+
+
+        public static MultiplayerSettings CreateMultiplayerSetting(bool isLinuxGameServer = true)
+        {
+            MultiplayerSettings settings = new MultiplayerSettings();
+            settings.TitleId = TestTitleId.ToString();
+            settings.BuildId = TestBuildId;
+
+            //MultiplayerSettings settings = JsonConvert.DeserializeObject<MultiplayerSettings>(File.ReadAllText(settingFilePath));
+            if (isLinuxGameServer)
+            {
+                settings.RunContainer = true;
+                settings.ContainerStartParameters = new ContainerStartParameters();
+            }
+            else
+            {
+                settings.RunContainer = false;
+                settings.ProcessStartParameters = new ProcessStartParameters();
+            }
+            return settings;
+        }
+
+        //public static MultiplayerSettings CreateMultiplayerSettingForLinux()
+        //{
+
+        //}
+        //public static ContainerStartParameters containerStartParamDetails()
+        //{
+        //    return new ContainerStartParameters { ImageDetails = new ContainerImageDetails() };
+        //}
+    }
+}
---
+++ VmAgent.Core.UnitTests/SessionHostContainerConfigurationTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Azure.Gaming.VmAgent.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VmAgentCoreInterface = Microsoft.Azure.Gaming.VmAgent.Core.Interfaces;
+using Docker.DotNet;
+using Microsoft.Azure.Gaming.AgentInterfaces;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Microsoft.Azure.Gaming.LocalMultiplayerAgent.Config;
+using System;
+using Microsoft.Azure.Gaming.VmAgent.Core.Interfaces;
+
+namespace VmAgent.Core.UnitTests
+{
+    [TestClass]
+    public class SessionHostContainerConfigurationUnitTests
+    {
+        private string _root = "C:\\";
+        private string _directoryPath;
+        private string _subdirectoryPath;
+        private string _directoryFilePath;
+        private string _subdirectoryFilePath;
+        private string _subdirectoryName = "subdirectory";
+        private string _directoryName = "directory";
+        private string _directoryFileName = "file";
+        private string _subdirectoryFileName = "subfile";
+        
+        private static Guid TestTitleIdUlong = Guid.NewGuid();
+        private static Guid TestBuildId = Guid.NewGuid();
+        private static Guid TestRegion = Guid.NewGuid();
+        private static string TestLogFolderId = "TestLogFolderId";
+        private static string TestPublicIpV4Address = "127.0.0.1";
+
+        private DirectoryInfo _subDirectoryInfo;
+        private DirectoryInfo _directoryInfo;
+        private VmConfiguration _vmConfiguration;
+        private MultiLogger _logger;
+        private Mock<VmAgentCoreInterface.ISystemOperations> _systemOperations;
+        private Mock<VmAgentCoreInterface.ISessionHostManager> _sessionHostManager;
+        private Mock<IDockerClient> _dockerClient;
+        private SessionHostsStartInfo _sessionHostsStartInfo;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            _directoryPath = Path.Combine(_root, _directoryName);
+            _directoryInfo = new DirectoryInfo(_directoryPath);
+            _subdirectoryPath = Path.Combine(_directoryPath, _subdirectoryName);
+            _subDirectoryInfo = new DirectoryInfo(_subdirectoryPath);
+            _directoryFilePath = Path.Combine(_directoryPath, _directoryFileName);
+            FileInfo directoryFileInfo = new FileInfo(_directoryFilePath);
+            _subdirectoryFilePath = Path.Combine(_subdirectoryPath, _subdirectoryFileName);
+            FileInfo subDirectoryFileInfo = new FileInfo(_subdirectoryFilePath);
+            //_defaultSourceFile = Path.Combine(_root, "Source");
+            string AssignmentId = $"{TestTitleIdUlong}:{TestBuildId}:{TestRegion}";
+
+            _logger = new MultiLogger(NullLogger.Instance, new TelemetryClient(TelemetryConfiguration.CreateDefault()));
+            _systemOperations = new Mock<Microsoft.Azure.Gaming.VmAgent.Core.Interfaces.ISystemOperations>();
+            _dockerClient = new Mock<IDockerClient>();
+            _sessionHostManager = new Mock<ISessionHostManager>();
+            _vmConfiguration = new VmConfiguration(56001, "vmid", new VmDirectories("root"), true);
+            
+            _sessionHostsStartInfo = new SessionHostsStartInfo();
+            _sessionHostsStartInfo.AssignmentId = AssignmentId;
+
+            _sessionHostManager.Setup(x => x.VmAgentSettings).Returns(new VmAgentSettings() { EnableCrashDumpProcessing = false });
+
+        }
+
+        [TestMethod]
+        [TestCategory("BVT")]
+        public void TestEnvVariablesWithSessionProcessOnWindow()
+        {
+            _sessionHostsStartInfo.SessionHostType = SessionHostType.Process;
+            _sessionHostsStartInfo.PublicIpV4Address = TestPublicIpV4Address;
+
+            SessionHostContainerConfiguration sessionHostContainerConfiguration =
+                new SessionHostContainerConfiguration(_vmConfiguration, _logger, _systemOperations.Object, _dockerClient.Object, _sessionHostsStartInfo, _sessionHostManager.Object);
+            IDictionary<string, string> envVariables = 
+                sessionHostContainerConfiguration.GetEnvironmentVariablesForSessionHost(0, TestLogFolderId, _sessionHostManager.Object.VmAgentSettings);
+            string log = envVariables["PF_SERVER_LOG_DIRECTORY"];
+            
+            Assert.IsFalse(log == "");
+        }
+
+        [TestMethod]
+        [TestCategory("BVT")]
+        public void TestEnvVariablesWithSessionContainerOnWindow()
+        {
+            _sessionHostsStartInfo.SessionHostType = SessionHostType.Container;
+
+            SessionHostContainerConfiguration sessionHostContainerConfiguration =
+                new SessionHostContainerConfiguration(_vmConfiguration, _logger, _systemOperations.Object, _dockerClient.Object, _sessionHostsStartInfo, _sessionHostManager.Object);
+            IDictionary<string, string> envVariables =
+                sessionHostContainerConfiguration.GetEnvironmentVariablesForSessionHost(0, "LogFolderId", _sessionHostManager.Object.VmAgentSettings);
+            string log = envVariables["PF_SERVER_LOG_DIRECTORY"];
+
+            Assert.IsFalse(log == "");
+        }
+
+        public void validateEnvironmentVariablesForSessionHost(IDictionary<string, string> envVariables, SessionHostType HostType)
+        {
+
+        }
+    }
+}

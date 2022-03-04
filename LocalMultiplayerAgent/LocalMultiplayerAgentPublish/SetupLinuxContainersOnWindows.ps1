ec74ffadbbcde0a763aae3dc83a1a0b55098b93e
Rakesh Varna <ravarna@microsoft.com>
2022-03-04 11:08:30 -0800

    Add html and other files to be excluded from being bundled into single file/

---
+++ LocalMultiplayerAgent/LocalMultiplayerAgentPublish/SetupLinuxContainersOnWindows.ps1
@@ -0,0 +1,6 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# This file is to support Linux Containers development on a Windows host using Docker for Windows
+# Create a custom docker network used by containers to talk to the Agent.
+docker network create playfab --driver bridge

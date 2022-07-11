6476379796c4c03dfc1c5731a82c9a4e0b24280d
emmanuel-jr <t-emidun@microsoft.com>
2022-07-11 15:46:14 -0700

    renamed deployment settings

--- LocalMultiplayerAgent/DeploymentTool/DeploymentSettings.cs
+++ LocalMultiplayerAgent/DeploymentTool/DeploymentSettings.cs
@@ -16,8 +16,8 @@ namespace Microsoft.Azure.Gaming.LocalMultiplayerAgent.DeploymentTool
 
         public string OSPlatform { get; set; }
 
-        public List<string> AssetFileNames { get; set; }
+        public List<string> AssetFileNames { get; } = new List<string>();
 
-        public List<BuildRegionParams> RegionConfigurations { get; set; }
+        public List<BuildRegionParams> RegionConfigurations { get; } = new List<BuildRegionParams>();
     }
 }

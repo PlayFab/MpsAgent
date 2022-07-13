34eda89956855f30f4e3fd9f528015b9605d4d0c
Brian Porter <bport@microsoft.com>
2022-07-13 11:18:07 -0700

    added ShutdownRequestedNotBefore VmAgent setting, and added clone constructors to MaintenanceSchedule (#125)

--- AgentInterfaces/MaintenanceSchedule.cs
+++ AgentInterfaces/MaintenanceSchedule.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Azure.Gaming.AgentInterfaces
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Newtonsoft.Json;
 
     // Data Format: https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-scheduled-events
@@ -16,6 +17,18 @@ namespace Microsoft.Azure.Gaming.AgentInterfaces
         public IList<MaintenanceEvent> MaintenanceEvents { get; set; }
 
         public string ReportingVmId { get; set; }
+
+        public MaintenanceSchedule() { }
+
+        /// <summary>
+        /// Deep copy used for testing
+        /// </summary>
+        public MaintenanceSchedule(MaintenanceSchedule other)
+        {
+            DocumentIncarnation = other.DocumentIncarnation;
+            MaintenanceEvents = other.MaintenanceEvents.Select((e) => new MaintenanceEvent(e)).ToList();
+            ReportingVmId = other.ReportingVmId;
+        }
     }
 
     // https://docs.microsoft.com/en-us/azure/virtual-machines/windows/scheduled-events#query-for-events
@@ -37,5 +50,22 @@ namespace Microsoft.Azure.Gaming.AgentInterfaces
         public string EventSource { get; set; }
 
         public int DurationInSeconds { get; set; }
+
+        public MaintenanceEvent() { }
+
+        /// <summary>
+        /// Deep copy used for testing
+        /// </summary>
+        public MaintenanceEvent(MaintenanceEvent other)
+        {
+            EventId = other.EventId;
+            EventType = other.EventType;
+            ResourceType = other.ResourceType;
+            AffectedResources = other.AffectedResources.ToList();
+            EventStatus = other.EventStatus;
+            NotBefore = other.NotBefore;
+            EventSource = other.EventSource;
+            DurationInSeconds = other.DurationInSeconds;
+        }
     }
 }

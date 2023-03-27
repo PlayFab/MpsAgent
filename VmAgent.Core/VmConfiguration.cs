24c19f7a9dc466e063845e4e0637abd74a1584d4
Dimitris Gkanatsios <dgkanatsios@outlook.com>
2023-03-27 10:38:25 -0700

    ordering VmStartupScriptPorts by NodePort

--- VmAgent.Core/VmConfiguration.cs
+++ VmAgent.Core/VmConfiguration.cs
@@ -150,8 +150,9 @@ namespace Microsoft.Azure.Gaming.VmAgent.Core
 
             if (sessionHostsStartInfo.VmStartupScriptConfiguration?.Ports != null)
             {
+                // order them by NodePort (VmPort) so that the lowest port is always the first one
                 int index = 0;
-                foreach (VmStartupScriptPort port in sessionHostsStartInfo.VmStartupScriptConfiguration.Ports)
+                foreach (VmStartupScriptPort port in sessionHostsStartInfo.VmStartupScriptConfiguration.Ports.OrderBy(x => x.NodePort))
                 {
                     environmentVariables.Add($"PF_STARTUP_SCRIPT_PORT_NAME_{index}", port.Name);
                     environmentVariables.Add($"PF_STARTUP_SCRIPT_PORT_EXTERNAL_{index}", port.PublicPort.ToString());

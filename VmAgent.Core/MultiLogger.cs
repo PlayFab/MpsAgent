48ce45d57ac89334aff7655bc46950d708a90446
Emma Park <emmapark@microsoft.com>
2023-06-15 14:07:57 -0700

    fixed another indentation issue

--- VmAgent.Core/MultiLogger.cs
+++ VmAgent.Core/MultiLogger.cs
@@ -21,9 +21,9 @@ namespace Microsoft.Azure.Gaming.VmAgent.Core
         {
             _logger = logger ?? throw new ArgumentException("logger cannot be null");
 
-			// To enable both GenevaAgentChannel and OpenTelemetry, we have 2 ILogger parameters here.
+            // To enable both GenevaAgentChannel and OpenTelemetry, we have 2 ILogger parameters here.
             // We will replace GenevaAgentchannel with Opentelemetry, once we verify all logs both in test and prod environments.
-			_genevaTelemetryClient = genevaTelemetryClient ?? throw new ArgumentException("genevaTelemetryClient cannot be null");
+            _genevaTelemetryClient = genevaTelemetryClient ?? throw new ArgumentException("genevaTelemetryClient cannot be null");
             _genevaOTelLogger = genevaLogger;
         }
 

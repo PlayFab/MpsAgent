8aae4b0013c66cf2348d7aeeb9c8386d3af59941
Jose Valenciano <jvalenciano@microsoft.com>
2023-10-03 15:56:19 -0600

    Add SecretDetails to SessionHostStartInfo

---
+++ AgentInterfaces/SecretDetail.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Azure.Gaming.AgentInterfaces
+{
+    using System;
+
+    public class SecretDetail
+    {
+        /// <summary>
+        /// Identifier secret
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Secret Value
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// The version of this secret in the internal Key Vault
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Expiration date of this secret
+        /// </summary>
+        public string ExpirationDate { get; set; }
+
+        /// <summary>
+        /// Expiration date of the secret.
+        /// </summary>
+        public DateTime ExpirationTimeUtc { get; set; }
+    }
+}

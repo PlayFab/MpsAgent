7c3b7a49e921e00ee635bfa14b1f6bef1120e20a
Thomas Culotta <tculotta@microsoft.com>
2024-03-20 14:44:49 -0700

    VmAgent endpoint to report GSDK version telemetry

---
+++ LocalMultiplayerAgent/Instrumentation/GsdkVersionInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalMultiplayerAgent.Instrumentation
+{
+    public class GsdkVersionInfo
+    {
+        public string Flavor { get; set; } // Unreal/Unity/C#/C++ etc.
+        public string Version { get; set; }
+    }
+}

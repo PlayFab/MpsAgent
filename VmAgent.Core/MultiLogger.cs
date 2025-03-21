6a96eb2c065e208743a43de35be841eae0394ca3
Jose Valenciano <jvalenciano@microsoft.com>
2025-03-21 15:03:19 -0600

    Add flag to decide if we sanitize or not the message

--- VmAgent.Core/MultiLogger.cs
+++ VmAgent.Core/MultiLogger.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System;
@@ -19,60 +16,60 @@ namespace Microsoft.Azure.Gaming.VmAgent.Core
             _genevaOtelLogger = genevaOpenTelemetryLogger;
         }
 
-        public void LogVerbose(string message)
+        public void LogVerbose(string message, bool sanitize = false)
         {
-            message = LogSanitizer.Sanitize(message);
+            message = SanitizeMessage(message, sanitize);
             _logger.LogInformation(message);
             _genevaOtelLogger?.LogInformation(message);
         }
 
-        public void LogInformation(string message)
+        public void LogInformation(string message, bool sanitize = false)
         {
-            message = LogSanitizer.Sanitize(message);
+            message = SanitizeMessage(message, sanitize);
             _logger.LogInformation(message);
             _genevaOtelLogger?.LogInformation(message);
         }
 
-        public void LogWarning(string message)
+        public void LogWarning(string message, bool sanitize = false)
         {
-            message = LogSanitizer.Sanitize(message);
+            message = SanitizeMessage(message, sanitize);
             _logger.LogWarning(message);
             _genevaOtelLogger?.LogWarning(message);
         }
 
-        public void LogError(string message)
+        public void LogError(string message, bool sanitize = false)
         {
-            message = LogSanitizer.Sanitize(message);
+            message = SanitizeMessage(message, sanitize);
             _logger.LogError(message);
             _genevaOtelLogger?.LogError(message);
         }
 
-        public void LogException(Exception exception)
+        public void LogException(Exception exception, bool sanitize = false)
         {
-            string message = LogSanitizer.Sanitize(exception.ToString());
+            string message = SanitizeMessage(exception.ToString(), sanitize);
             _logger.LogError(message);
             _genevaOtelLogger?.LogError(message);
         }
 
-        public void LogException(string message, Exception exception)
+        public void LogException(string message, Exception exception, bool sanitize = false)
         {
-            string logMessage = LogSanitizer.Sanitize($"{message}. Exception: {exception}");
+            string logMessage = SanitizeMessage($"{message}. Exception: {exception}", sanitize);
             _logger.LogError(logMessage);
             _genevaOtelLogger?.LogError(logMessage);
         }
 
-        public void LogEvent(string eventName, IDictionary<string, string> properties, IDictionary<string, double> metrics)
+        public void LogEvent(string eventName, IDictionary<string, string> properties, IDictionary<string, double> metrics, bool sanitize = false)
         {
             string propertiesString = properties == null ? CommonSettings.NullStringValue : JsonConvert.SerializeObject(properties, CommonSettings.JsonSerializerSettings);
             string metricsString = metrics == null ? CommonSettings.NullStringValue : JsonConvert.SerializeObject(metrics, CommonSettings.JsonSerializerSettings);
-            string message = LogSanitizer.Sanitize($"Event: {eventName}. Properties: {propertiesString}, Metrics: {metricsString}");
+            string message = SanitizeMessage($"Event: {eventName}. Properties: {propertiesString}, Metrics: {metricsString}", sanitize);
             _logger.LogInformation(message);
             _genevaOtelLogger?.LogInformation(message);
         }
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            string message = LogSanitizer.Sanitize(formatter(state, exception));
+            string message = formatter(state, exception);
             _logger.Log(logLevel, eventId, state, exception, (s, e) => message);
         }
 
@@ -84,6 +81,11 @@ namespace Microsoft.Azure.Gaming.VmAgent.Core
         public IDisposable BeginScope<TState>(TState state)
         {
             return _logger.BeginScope(state);
+        }
+
+        private string SanitizeMessage(string message, bool sanitize)
+        {
+            return sanitize ? LogSanitizer.Sanitize(message) : message;
         }
     }
 }
